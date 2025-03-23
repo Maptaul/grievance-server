76a89662d4e3eed2e07285702debc68df917b853
@maptaul <maptaulislam1@gmail.com>
2025-03-23 15:25:04 +0600

    b

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    //     await client.connect();
 
     const usersCollection = client.db("grievance").collection("users");
     const categoryCollection = client.db("grievance").collection("category");
